8a0752907aabd63897fe2a4a38f1b935c91932f7
JoelMedina123 <joelestebanmedina@gmail.com>
2026-02-10 13:17:46 -0500

    Add files via upload

--- Corte1/Teoria/practica3/practica3.py
+++ Corte1/Teoria/practica3/practica3.py
@@ -1,11 +1,20 @@
 while True:
   a = input("porfavor ingrese un valor: ")
-
-  ain12t = int(a)
+  aint =int(a)
   mod = aint%2
 
   if(mod == 0):
      print("a es un PAR")
   else:
      print("a no es PAR, es IMPAR")
-
+  b = input ("¿Quieres contuniar?, Si(S) No(N): ")
+  if b == "N":
+     print("Terminamos. ")
+     break
+  if b == "S":
+     print("continuamos: ")
+  else:
+     print("su respuesta no es valida")
+  
+     
+  
