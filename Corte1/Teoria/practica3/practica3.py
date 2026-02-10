c810677255db0df2cfee2e963e5ef725f10feddb
JoelMedina123 <joelestebanmedina@gmail.com>
2026-02-10 12:54:52 -0500

    Add files via upload

---
+++ Corte1/Teoria/practica3/practica3.py
@@ -0,0 +1,11 @@
+while True:
+  a = input("porfavor ingrese un valor: ")
+
+  ain12t = int(a)
+  mod = aint%2
+
+  if(mod == 0):
+     print("a es un PAR")
+  else:
+     print("a no es PAR, es IMPAR")
+
